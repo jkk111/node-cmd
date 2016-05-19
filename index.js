e8c86c0f7322e349aff48739b39161eedd50682e
John Kelly <kellyj58@tcd.ie>
2016-05-19 23:05:57 +0100

    Removed some unnecessary code, added comments

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var LINE_ENDING = process.platform === "win32" ? "\r\n" : "\n";
 var spawn = require("child_process").spawn;
-var config;
-try {
-  config = JSON.parse(fs.readFileSync("config.json"));
-} catch(e) {
 
-}
-
+// Executes a specified command in a prompt.
 function executeCommand(prompt, command) {
   console.log("Executing", command)
   prompt.stdin.write(command + LINE_ENDING);
 }
 
+// Returns an instance of the correct prompt.
 function spawnPrompt() {
   if(process.platform === "win32")
     return spawn("cmd");
@@ -19,34 +15,19 @@ function spawnPrompt() {
     return spawn("bash");
 }
 
-module.exports = function(http) {
-  var io = require("socket.io")(http);
-  initSocketListener(io);
-}
-
+// Starts socket listener.
 function initSocketListener(io) {
   io.on("connection", function(socket) {
     var prompt = spawnPrompt();
-    // Start of requesting credentials from server.
-    //
-    // if(!config) {
-    //   socket.emit("request-credentials");
-    // }
-    //
-    // socket.on("credentials", function(data) {
-    //   fs.writeFileSync("config.json", JSON.stringify(data, null, "  "), "utf8");
-    //   config = data;
-    // });
-
     prompt.stdout.on("data", function(data) {
       console.log("response:", data.toString().trim());
       socket.emit("command-result", data);
-    })
+    });
 
     prompt.stderr.on("data", function(data) {
       console.log("error:", data.toString().trim());
       socket.emit("command-error", data);
-    })
+    });
 
     socket.on("command", function(command) {
       if(!prompt)
@@ -58,4 +39,8 @@ function initSocketListener(io) {
       prompt.kill();
     });
   });
+}
+
+module.exports = function(http) {
+  initSocketListener(require("socket.io")(http));
 }
